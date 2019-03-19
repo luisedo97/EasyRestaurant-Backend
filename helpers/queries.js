7cd0b5c12dfda5626f1f5b624c41cd11443ec988
Luis E. Araujo B <luiseduardoab97@users.noreply.github.com>
2019-03-18 21:13:33 -0400

    correction bug

--- helpers/queries.js
+++ helpers/queries.js
@@ -1,7 +1,7 @@
 const PS = require('pg-promise').PreparedStatement;
 let queries = {
-  newUser: new PS('new-user', "INSERT INTO users (user_username, user_password, user_name, user_lastname, type_user_id) VALUES ($1,$2,$3,$4,$5)"),
-  getUser: new PS('get-user', "SELECT * FROM USERS WHERE user_username = $1"),
+  newUser: new PS('new-user', "INSERT INTO users (user_username, user_password, user_name, user_lastname, type_users_id) VALUES ($1,$2,$3,$4,$5)"),
+  getUser: new PS('get-user', "SELECT * FROM users WHERE user_username = $1"),
 }
 
 module.exports = queries;
